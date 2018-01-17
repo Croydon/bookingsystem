32629cd2ae2397ffd425f2ccb0780b0e7f1396a1
Croydon <cr0ydon@outlook.com>
2018-01-17 12:01:15 +0100

    Implement menu and exit

--- src/main/java/bookingsystem/Bookingsystem.java
+++ src/main/java/bookingsystem/Bookingsystem.java
@@ -1,8 +1,34 @@
 package bookingsystem;
 
+import java.util.Scanner;
+
 public class Bookingsystem {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO: code application logic here
+        int option;
+        while (true) {
+            // Prints the menu and allows user to choose
+            System.out.println("");
+            System.out.println("1. Kundennummer eingeben");
+            System.out.println("2. Flugziel angeben");
+            System.out.println("3. Flug buchen");
+            System.out.println("0: Beenden");
+
+            System.out.println("");
+            System.out.println("Menüpunkt eingeben: ");
+            option = scanner.nextInt();
+            System.out.println("");
+
+            switch (option) {
+                case 0:
+                    System.exit(0);
+
+                default:
+                    System.out.println("Ungültige Eingabe!");
+            }
+
+        }
     }
 }
